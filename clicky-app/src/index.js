7f951e109b9a327c48e1ca364459f4e67537c641
Batli Joselevitz <batlijoselevitz@Batlis-MBP.attlocal.net>
2018-02-22 13:26:10 -0600

    and the tinkering begins

--- clicky-app/src/index.js
+++ clicky-app/src/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Layout extends React.Component {
+  render(){
+    return (
+      <h1>It works!</h1>
+    );
+  }
+}
+
+
+
+
+
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app)
